7e7b92aa6740cf8ffb512ad5f9716fa25f406e66
Голушко Екатерина <ekaterinagolushko@yandex.ru>
2021-09-29 12:37:55 +0300

    1

---
+++ pz_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int k = 0, z = 1, i = 0;
+            while (x >= z)
+            {
+                (k, z) = (z, k + z);
+                i++;
+            }
+            if (x == k) Console.WriteLine(i);
+            else Console.WriteLine(-1);
+        }
+    }
+}
