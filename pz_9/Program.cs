be2047674f7181b302248b523b69b11b7deb0cec
Голушко Екатерина <ekaterinagolushko@yandex.ru>
2021-10-15 13:13:21 +0300

    pz_9

---
+++ pz_9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace pz_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число от 0 до 10: ");
+            int inputNum = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            int sizeArray = random.Next(8, 20);
+            int[,] array = new int[sizeArray, sizeArray];
+            for (int i = 0; i < sizeArray; i++)
+            {
+                for (int j = 0; j < sizeArray; j++)
+                {
+                    array[i, j] = random.Next(0, 10);
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < sizeArray; i++)
+            {
+                for (int j = 0; j < sizeArray; j++)
+                {
+                    if (array[i, j] == inputNum) count++;
+                }
+            }
+            Console.WriteLine("Таблица");
+            for (int i = 0; i < sizeArray; i++)
+            {
+                for (int j = 0; j < sizeArray; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Кол-во повторяющихся элементов " + inputNum + " равна " + count);
+        }
+    }
+}
