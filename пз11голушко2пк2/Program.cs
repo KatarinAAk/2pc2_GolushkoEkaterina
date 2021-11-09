4633c44107ae504a9d68d9f280bb5c29885479f7
Голушко Екатерина <ekaterinagolushko@yandex.ru>
2021-11-09 07:37:13 +0300

    голушко

---
+++ пз11голушко2пк2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace пз11голушко2пк2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string S = Console.ReadLine();
+            Console.Write("Введите число:");
+            int N = int.Parse(Console.ReadLine());
+            string str = "";
+            foreach (char i in S)
+            {
+                str += new string('*', N) + i;
+            }
+            str += new string('*', N);
+            Console.WriteLine(str);
+
+        }
+    }
+}
