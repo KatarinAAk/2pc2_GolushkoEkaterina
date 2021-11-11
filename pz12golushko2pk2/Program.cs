75bb07b1d89b265d4804e40187f568428535fafb
Голушко Екатерина <ekaterinagolushko@yandex.ru>
2021-11-11 08:25:06 +0300

    привет

---
+++ pz12golushko2pk2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pz12golushko2pk2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine().ToUpper();
+            string[] array = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int count = 0;
+            foreach (string i in array)
+            {
+                if (i[0] == 'О') count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
