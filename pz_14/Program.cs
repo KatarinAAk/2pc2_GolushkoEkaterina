bb8322735d4e374a052963421f2ce853c8b38d8f
Голушко Екатерина <ekaterinagolushko@yandex.ru>
2021-11-23 21:15:19 +0300

    pz_14

---
+++ pz_14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace pz_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PATH = @"C:\Users\golushko\Desktop\999.txt";
+            string str;
+
+            Console.Write("Введите число:");
+            int num = int.Parse(Console.ReadLine());
+
+            FileStream file = new FileStream(PATH, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter writer = new StreamWriter(file);
+
+            for (int i = 0; i < num; i++)
+            {
+                str = new string('*', i);
+                writer.WriteLine(str);
+            }
+            writer.Close();
+        }
+    }
+}
