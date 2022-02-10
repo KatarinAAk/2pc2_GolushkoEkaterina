663af94833019f072e82a3396012ac57a654656f
Голушко Екатерина <ekaterinagolushko@yandex.ru>
2022-02-10 17:48:58 +0300

    1

---
+++ pz__2.3/Package.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pz__2._3
+{
+    class Package
+    {
+        public string nameDest;
+        public string nameSor;
+        public string addres;
+        public string type;
+        public DateTime receiptDate;
+
+        public Package(string _nameDest, string _nameSor, string _addres, string _type)
+        {
+            nameDest = _nameDest;
+            nameSor= _nameSor;
+            addres = _addres;
+            type = _type;
+            receiptDate = new DateTime(2004,02,05,12,55,54);
+        }
+
+        public void GetPackageInfo()
+        {
+            Console.WriteLine($"nameDest = {nameDest}, nameSor = {nameSor}, addres = {addres}, receiptDate = {receiptDate}, type = {type}");
+        }
+    }
+}
---
+++ pz__2.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pz__2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Package p1 = new Package("Вика", "Рэм", "г.Екатеренбург", "Подарок");
+            Package p2 = new Package("Арсений", "Тамара", "г.Тамбов", "Деньги");
+            Package p3 = new Package("Лев", "Люда", "г.Оренбург", "Письмо");
+            Package p4 = new Package("Катарина", "Анна", "г.Москва", "Деньги");
+
+            p1.GetPackageInfo();
+            p2.GetPackageInfo();
+            p3.GetPackageInfo();
+            p4.GetPackageInfo();
+        }
+    }
+}
